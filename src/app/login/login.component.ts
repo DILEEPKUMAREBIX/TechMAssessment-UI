40ad8b45b015f7c7a6c256de5b75398706267e49
DILEEP KUMAR KOTTAKOTA <kottakotadileepkumar@gmail.com>
2019-06-04 12:32:34 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProfileService } from '../profile/profile.service';
 
 @Component({
   selector: 'login',
@@ -10,42 +11,50 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginComponent {
   isLogin: boolean = true;
-  message:string = '';
-  showMessage:boolean = false;
+  message: string = '';
+  showMessage: boolean = false;
   loginForm: FormGroup;
   signUpForm: FormGroup;
   alertType: string;
 
-  constructor(public service: LoginService, public router:Router, public fb:FormBuilder) {
+  constructor(public service: LoginService, public router: Router, public fb: FormBuilder, public profileService: ProfileService) {
     this.createForm()
+  }
+
+  ngOnInit() {
+    this.profileService.loggedInProfile = {};
+    this.profileService.isSubmitAction = false;
   }
 
   createForm() {
     this.loginForm = this.fb.group({
-      username: [''],
-      password: ['']
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
 
   createSignUpForm() {
     this.signUpForm = this.fb.group({
-      username: [''],
-      password: [''],
-      confirm: ['']
-    });
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirm: ['', [Validators.required]]
+      // password: ['Dil@763362', [Validators.required]],
+      // confirm: ['Dil@763362', [Validators.required]]
+    }
+    );
   }
 
 
   login() {
+    this.profileService.trim(this.loginForm);
     this.clearMessage();
     this.service.validate(this.loginForm.value).subscribe(
       (data: any) => {
-        // alert('success');
-        this.service.loggedInUser = data;
+        this.profileService.loggedInProfile = data;
         this.getUser(data.id);
       },
       (error) => {
-        this.setMessage('Invalid Credintials provided','danger');
+        this.setMessage(error['error']['message'], 'danger');
         this.loginForm.reset();
       });
   }
@@ -53,28 +62,49 @@ export class LoginComponent {
   getUser(id) {
     this.service.getUser(id).subscribe(
       (data) => {
-        // alert('success');
-        console.log(data);
+        this.profileService.loggedInProfile = data;
         this.router.navigate(['/', 'dashboard']);
       },
       (error) => {
+        this.profileService.isSubmitAction = true;
         this.router.navigate(['/', 'profile']);
       });
   }
 
+  validate() {
+    if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.signUpForm.value.email))) {
+      this.setMessage("Please enter valid email address", 'danger');
+      return false;
+    }
+    else if (!(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(this.signUpForm.value.password))) {
+      this.setMessage("Password should be atleast 8 characters long and should contain one number,one capital character and one special character", 'danger');
+      return false;
+    }
+    else if (this.signUpForm.value.password != this.signUpForm.value.confirm) {
+      this.setMessage("Passwords don't match", 'danger');
+      return false;
+    }
+    return true;
+  }
+
   saveLogin() {
-    this.service.saveUser(this.signUpForm.value).subscribe(
-      (data) => {
-        // alert('success');
-        this.createForm();
-        this.isLogin = true;
-        this.setMessage('You have registered successfully','success');
-      },
-      (error)=> {
-        // alert('error');
-        this.setMessage('Error in registering user','danger');
-      }
-    )
+    this.profileService.trim(this.signUpForm);
+    this.clearMessage();
+    if (this.validate()) {
+      this.service.saveUser(this.signUpForm.value).subscribe(
+        (data) => {
+          // alert('success');
+          this.createForm();
+          this.isLogin = true;
+          this.setMessage('You have registered successfully', 'success');
+        },
+        (error) => {
+          // alert('error');
+          this.setMessage(error.error.message, 'danger');
+        }
+      )
+    }
+
   }
 
   swithclogin(value) {
@@ -99,4 +129,11 @@ export class LoginComponent {
     this.message = '';
     this.showMessage = false;
   }
+
+  get f() {
+    if (this.signUpForm && this.signUpForm.controls)
+      return this.signUpForm.controls;
+  }
+
+
 }
